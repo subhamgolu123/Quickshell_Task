62859cc82bb1c032c9f7b96af52cd30b07d81249
shubham kumar <subhamgolu123@gmail.com>
2023-10-19 00:41:52 +0530

    Quiksell

---
+++ README.md
@@ -0,0 +1,5 @@
+## Project View
+
+<kbd>![](https://res.cloudinary.com/anshumxn09/image/upload/v1692799526/test/quickSell2_pjyru9.png)</kbd>
+
+<kbd>![](https://res.cloudinary.com/anshumxn09/image/upload/v1692799514/test/quickSell_nycbcs.png)</kbd>
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react'
+import './App.css';
+import TopNav from './components/TopNav/TopNav';
+import DashView from './components/DashBoard/DashView';
+import { useDispatch, useSelector} from 'react-redux'
+import { fetchAllData } from './Actions/DataAction';
+import Loading from './components/Loading/Loading';
+
+const App = () => {
+  const dispatch = useDispatch();
+  const {allTickets} = useSelector(state => state.DataReducer);
+   
+  useEffect(() => {
+    dispatch(fetchAllData());
+  }, [dispatch])
+
+  return allTickets ? (
+    <div style={{paddingTop : "10px"}} >
+      <TopNav/>
+      <hr style={{marginTop : "10px"}} />
+      <DashView/>
+    </div>
+  ) : <Loading/>
+}
+
+export default App
